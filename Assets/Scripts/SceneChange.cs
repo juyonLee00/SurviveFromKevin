d43842eef26979b901258139b351f0fecf74bd11
pjayas <115007528+pjayas@users.noreply.github.com>
2023-10-04 01:47:03 +0900

    addTriger

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private Color m_oldColor = Color.white;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        Renderer render = GetComponent<Renderer>();
+
+        m_oldColor = render.material.color;
+        render.material.color = Color.green;
+
+        SceneManager.LoadScene(2);
+    }
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Test")
+    //    {
+    //        Debug.Log(other.gameObject);
+    //        Renderer render = GetComponent<Renderer>();
+
+    //        m_oldColor = render.material.color;
+    //        render.material.color = Color.green;
+    //    }
+    //}
+}
